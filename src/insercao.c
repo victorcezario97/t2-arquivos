3310a81c8c4405fb3a657b3389e0a76cfbdbc56e
victorcezario97 <victorscezario@hotmail.com>
2017-07-01 22:51:54 -0300

    Add files via upload

--- src/insercao.c
+++ src/insercao.c
@@ -8,8 +8,69 @@ enum{
 	FIRST_FIT
 };
 
-int inserir(REGISTRO reg, int op){
+int recebeCampo(char **campo, boolean fixo, boolean nulo){
+	int n = 0;
+	while(n == 0){
+		(*campo) = leString(&n);
+
+		if(nulo == FALSE && fixo == TRUE){
+			if(n == TAMANHO_FIXO) break;
+
+			printf("Tamanho invalido para o campo\n");
+			if(n != 0) free((*campo));
+			n = 0;
+			continue;
+		}else if(nulo == TRUE && fixo == FALSE){
+			break;
+		}else if(nulo == TRUE && fixo == TRUE){
+			if(n == 0 || n == TAMANHO_FIXO) break;
+
+			printf("Tamanho invalido para o campo\n");
+			free((*campo));
+			n = 0;
+			continue;
+		}else{
+			if(n != 0) break;
+
+			printf("Tamanho invalido para o campo\n");
+			n = 0;
+		}
+	}
+
+	return n;
+}
+
+REGISTRO recebeRegistro(){
+	REGISTRO reg;
+	int n = 0;
+
+	printf("Insira os dados sobre o registro.\nAo fim de cada dado digite ENTER, e caso o dado seja nulo, digite apenas ENTER.\n");
+	printf("DOMINIO: ");
+	reg.tamanhoDominio = recebeCampo(&reg.dominio, FALSE, FALSE);
+	printf("\nDOCUMENTO: ");
+	recebeCampo(&reg.documento, TRUE, FALSE);
+	printf("\nNOME: ");
+	reg.tamanhoNome = recebeCampo(&reg.nome, FALSE, FALSE);
+	printf("\nUF: ");
+	reg.tamanhoUF = recebeCampo(&reg.uf, FALSE, FALSE);;
+	printf("\nCIDADE: ");
+	reg.tamanhoCidade = recebeCampo(&reg.cidade, FALSE, TRUE);;
+	printf("\nDATA E HORA DO CADASTRO: ");
+	recebeCampo(&reg.dataHoraCadastro, TRUE, TRUE);
+	printf("\nDATA E HORA DA ATUALIZACAO: ");
+	recebeCampo(&reg.dataHoraAtualizacao, TRUE, TRUE);
+	printf("\nTICKET: ");
+	scanf("%d", &reg.ticket);
+	printf("\n");
+
+	return reg;
+}
+
+int inserir(){
 	int pos;
+	REGISTRO reg;
+
+	reg = recebeRegistro();
 
 
 	return pos;
