81cf6836f66afd8f5d43d7deb63eac8661e4b6d8
victorcezario97 <victorscezario@hotmail.com>
2017-07-04 17:40:38 -0300

    Add files via upload

--- include/listaRem.h
+++ include/listaRem.h
@@ -1,7 +1,10 @@
 #ifndef _LISTAREM_H_
 #define _LISTAREM_H_
 
-int encontraPos(int n, FILE *arquivo);
+#include <utils.h>
+
+void removeLista(int bo, FILE *arquivo);
+int encontraPos(int n, FILE *arquivo, boolean op, int *end, boolean *reordena);
 void ordenaBest(FILE *arquivo, int rrn, int tam, int pos);
 void ordenaWorst(FILE *arquivo, int rrn, int tam, int pos);
 void ordenaFirst(FILE *arquivo, int rrn, int pos);
